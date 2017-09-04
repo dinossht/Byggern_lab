02fb5058127ec865a21914d1e5b427bf2d2ddc65
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-09-04 10:30:02 +0200

    Moved func to c-file

---
+++ Byggern/Byggern/src/UART_driver/UART_driver.c
@@ -0,0 +1,54 @@
+/*
+ * UART_driver.c
+ *
+ * Created: 04.09.2017 09:28:55
+ *  Author: dinossht
+ */ 
+
+
+#include "settings.h"
+#include "UART_driver.h"
+
+#define BAUD 9600
+#define MYUBRR F_CPU / 16 / BAUD - 1 // Baud Rate Register Setting
+
+// UART0 Initialization routine
+void UART0_Init()
+{
+	/* Set UART baudrate */
+	UBRR0H = (unsigned char)(MYUBRR >> 8);
+	UBRR0L = (unsigned char)MYUBRR;
+	
+	/* Enable reciever and transimitter */
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+	
+	/* Frame format: 8data */
+	UCSR0C = (1 << URSEL0) | (1 << UCSZ00) | (1 << UCSZ01);
+	
+	/* 2 stop bit */
+	UCSR0C |= (1 << USBS0);
+	
+	/* Clock polarity must be set to 0 for asynchronous mode */
+	UCSR0C &= ~(1 << UCPOL0);
+}
+
+// UART transmission routine
+void UART0_Transmit(unsigned char data)
+{
+	/* Wait for empty transmit buffer */
+	while (!(UCSR0C & (1 << UDRE0)));
+	
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+// UART recieve routine
+unsigned char UART0_Receive()
+{
+	/* Wait for data to be received */
+	while (!(UCSR0C & (1 << RXC0)));
+	
+	/* Get and return received data from buffer */
+	return UDR0;
+}
+
--- Byggern/Byggern/src/UART_driver/UART_driver.h
+++ Byggern/Byggern/src/UART_driver/UART_driver.h
@@ -9,52 +9,10 @@
 #ifndef UART_DRIVER_H_
 #define UART_DRIVER_H_
 
-// #define F_CPU 8000000UL	// Internal clock speed
-#define BAUD 9600
-#define MYUBRR F_CPU / 16 / BAUD - 1 // Baud Rate Register Setting
+#include <asf.h>
 
-void UART_Init(void);
-void UART_Transmit(unsigned char data);
-unsigned char UART_Receive(void);
-
-// UART Initialization routine
-void UART_Init()
-{
-	/* Set UART baudrate */
-	UBRR1H = (unsigned char) (MYUBRR >> 8);
-	UBRR1L = (unsigned char) MYUBRR;
-	
-	/* Enable reciever and transimitter */
-	UCSR1B = (1 << TXEN1) | (1 << RXEN1);
-	
-	/* Frame format: 8data */
-	UCSR1C = (1 << URSEL1) | (1 << UCSZ10) | (1 << UCSZ00);
-	
-	/* 2 stop bit */
-	UCSR1C |= (1 << USBS1);
-	
-	/* Clock polarity must be set to 0 for asynchronous mode */
-	UCSR1C &= ~(1 << UCPOL0);
-}
-?
-// UART transmission routine
-void UART_Transmit(unsigned char data)
-{
-	/* Wait for empty transmit buffer */
-	while (!(UCSR0C & (1 << UDRE0)));
-	
-	/* Put data into buffer, sends the data */
-	UDR0 = data;
-}
-?
-// UART recieve routine
-unsigned char UART_Receive()
-{
-	/* Wait for data to be received */
-	while (!(UCSR0C & (1 << RXC0)));
-	
-	/* Get and return received data from buffer */
-	return UDR0;
-}
+void UART0_Init(void);
+void UART0_Transmit(unsigned char data);
+unsigned char UART0_Receive(void);
 
 #endif /* UART_DRIVER_H_ */
