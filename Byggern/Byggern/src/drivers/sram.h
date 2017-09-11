b14a0bbe63e4d68e7bee00304ad7486f9dca8f72
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-09-11 20:34:13 +0200

    added SRAM test

---
+++ Byggern/Byggern/src/drivers/sram.h
@@ -0,0 +1,15 @@
+/*
+ * sram.h
+ *
+ * Created: 11.09.2017 08:30:31
+ *  Author: dinossht
+ */ 
+
+
+#ifndef SRAM_H_
+#define SRAM_H_
+
+void SRAM_test(void);
+void SRAM_test_2(void);
+
+#endif /* SRAM_H_ */
