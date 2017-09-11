b0f98c17b39628419de63bc84b8a36f57c4beeab
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-09-11 20:31:29 +0200

    added address solver
    testing muxing by writing to external memory

---
+++ Byggern/Byggern/src/drivers/address_solver.h
@@ -0,0 +1,16 @@
+/*
+ * address_solver.h
+ *
+ * Created: 11.09.2017 14:08:07
+ *  Author: dinossht
+ */ 
+
+
+#ifndef ADDRESS_SOLVER_H_
+#define ADDRESS_SOLVER_H_
+
+
+void address_solver_test();
+
+
+#endif /* ADDRESS_SOLVER_H_ */
