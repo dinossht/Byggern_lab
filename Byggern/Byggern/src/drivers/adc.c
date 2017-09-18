4b9eb4b8298cdf0b55f77de908d24e8fbb0c586c
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-09-18 07:28:12 +0200

    Add adc

---
+++ Byggern/Byggern/src/drivers/adc.c
@@ -0,0 +1,22 @@
+/*
+ * adc.c
+ *
+ * Created: 11.09.2017 08:30:18
+ *  Author: dinossht
+ */ 
+
+#include <stdlib.h>
+#include "stdio.h"
+#include "uart.h"
+
+void adc_init()
+{
+	// configure the ADC
+	// Enable interrupt functionn in ADC
+	// 8-bit results
+	// Enable a prescaler - determined by the internal or external clock
+	
+	// Turn on the ADC feature
+	
+	// Start the first conversation
+}
---
+++ Byggern/Byggern/src/drivers/adc.h
@@ -0,0 +1,14 @@
+/*
+ * adc.h
+ *
+ * Created: 11.09.2017 08:30:31
+ *  Author: dinossht
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+void adc_init(void);
+
+#endif /* ADC_H_ */
