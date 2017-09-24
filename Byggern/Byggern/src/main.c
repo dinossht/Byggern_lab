4d5de6c96459dba42e355418dc2467a9a2e31cfa
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-09-25 01:19:27 +0200

    Add ex1, 2, 3 in main

--- Byggern/Byggern/src/main.c
+++ Byggern/Byggern/src/main.c
@@ -1,8 +1,9 @@
 #include <asf.h>
-
+#include "settings.h"
 #include "hardware_init.h"
 
-#include "exercises/ex02.h"
+// #include "exercises/ex01.h"
+// #include "exercises/ex02.h"
 #include "exercises/ex03.h"
 
 int main (void)
