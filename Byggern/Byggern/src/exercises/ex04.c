51bbc3baf180723b87b1c000f29bf779d80865a5
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-09-25 11:51:29 +0200

    Add ex04

---
+++ Byggern/Byggern/src/exercises/ex04.c
@@ -0,0 +1,6 @@
+/*
+ * ex04.c
+ *
+ * Created: 25.09.2017 11:49:09
+ *  Author: dinossht
+ */ 
---
+++ Byggern/Byggern/src/exercises/ex04.h
@@ -0,0 +1,16 @@
+/*
+ * ex04.h
+ *
+ * Created: 25.09.2017 11:48:52
+ *  Author: dinossht
+ */ 
+
+
+#ifndef EX04_H_
+#define EX04_H_
+
+
+
+
+
+#endif /* EX04_H_ */
