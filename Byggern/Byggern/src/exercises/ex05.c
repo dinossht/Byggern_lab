a60b273edf7d03c355beaf7c5c367c95fededc93
Unknown <hermann_sun@hotmail.com>
2017-10-02 12:37:30 +0200

    Add ex05

---
+++ Byggern/Byggern/src/exercises/ex05.c
@@ -0,0 +1,6 @@
+/*
+ * ex05.c
+ *
+ * Created: 02.10.2017 11:55:04
+ *  Author: hermansu
+ */ 
---
+++ Byggern/Byggern/src/exercises/ex05.h
@@ -0,0 +1,16 @@
+/*
+ * ex05.h
+ *
+ * Created: 02.10.2017 11:54:54
+ *  Author: hermansu
+ */ 
+
+
+#ifndef EX05_H_
+#define EX05_H_
+
+
+
+
+
+#endif /* EX05_H_ */
