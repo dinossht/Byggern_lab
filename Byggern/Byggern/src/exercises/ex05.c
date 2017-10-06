970d6bc10484b6e6d611fc175cfac4082025f3ea
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-10-06 22:12:02 +0200

    Add test function for buffer load and read

--- Byggern/Byggern/src/exercises/ex05.c
+++ Byggern/Byggern/src/exercises/ex05.c
@@ -5,32 +5,64 @@
  *  Author: hermansu
  */ 
 
-#include <asf.h>
-#include "../settings.h"
-#include <util/delay.h>
-
-#include "ex05.h"
-#include "../drivers/mcp2515.h"
-#include "../drivers/spi.h"
 #include <stdio.h>
 
+#include "../drivers/mcp2515.h"
+#include "../drivers/spi.h"
+#include "ex05.h"
 
-#define CAN_PORT PORTB
-#define CAN_CS PINB4
+
+static void mcp2515_printModeStatus(uint8_t status);
+static void mcp2515_testReadAndLoadBuffer(void);
 
 void ex05()
 {
-
-	
 	mcp2515_init();	
+	// loop back mode for testing
+	mcp2515_printModeStatus(mcp2515_readStatus());
 	
+	mcp2515_testReadAndLoadBuffer();
+}
 
-	while(1)
+static void mcp2515_printModeStatus(uint8_t status)
+{
+	printf("Current mode: ");
+	switch(status)
 	{
-		CAN_PORT &= ~(1 << CAN_CS); // Select CAN-controller
-		_delay_us(500);
-		spi_masterTransmit(4);	
-		CAN_PORT |= (1 << CAN_CS); // Deselect CAN-controller
- 		_delay_us(500);	
-	}	
+		case MODE_NORMAL:
+			printf("MODE NORMAL");
+			break;     
+			
+		case MODE_SLEEP:
+			printf("MODE SLEEP");
+			break;      
+			
+		case MODE_LOOPBACK:
+			printf("MODE LOOPBACK");
+			break;   
+			
+		case MODE_LISTENONLY:
+			printf("MODE LISTENONLY");
+			break;
+			
+		case MODE_CONFIG:
+			printf("MODE CONFIG");
+			break;     
+	}
+	printf("\n");
+}
+
+static void mcp2515_testReadAndLoadBuffer()
+{
+	uint8_t bufferTX[] = {0xCA, 0xFF, 0xEE};  // ABCDEF
+	uint8_t bufferRX[3];
+	
+	mcp2515_loadTX(MCP_TXB1CTRL + 2, bufferTX, 3);
+	mcp2515_readRX(MCP_TXB1CTRL + 2, bufferRX, 3);
+	
+	for(uint8_t i = 0; i < 3; i++)
+	{
+		printf("%2X", bufferRX[i]);
+	}
+	printf("?\n\n");
 }
