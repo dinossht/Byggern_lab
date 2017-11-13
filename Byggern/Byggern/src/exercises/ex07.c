ab3f0fffb5bc6a74cb860e9749057c7d4094ad52
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-13 16:36:27 +0100

    Add oled

--- Byggern/Byggern/src/exercises/ex07.c
+++ Byggern/Byggern/src/exercises/ex07.c
@@ -19,6 +19,7 @@
 #include "../drivers/mcp2515.h"
 #include "../drivers/spi.h"
 #include "../drivers/CAN.h"
+#include "../multiboard/oled.h"
 #include "../multiboard/joystick.h"
 #include "../multiboard/slider.h"
 #include "../multiboard/button.h"
@@ -52,7 +53,11 @@ void ex07()
 {
 	can_init();
 	joystick_calib();
+	oled_init();
+	oled_clear();
 	
+	int x = 0;
+	int y = 0;
 	while(1)
 	{
 		joystick_message.data.i8[0] = joystick_getPos(POS_X);
@@ -74,6 +79,24 @@ void ex07()
 		button_message.data.u8[1] = button_getStat(BUTTON_RIGHT);
 		can_message_send(&button_message);
 //		printf("%d\n", button_message.data.u8[0]);
+		
+		oled_print("Hei", 0, 0);
+		oled_setContrast(255);
+		
+		
+		
+// 		x++;
+// 
+// 		if(x == 127)
+// 		{
+// 			x = 0;
+// 			y++;
+// 		}
+// 		
+// 		
+// 		
+// 		oled_pixel(x, y);
+		
 		
 		_delay_ms(10);
 	}	
