5f21feb7949a373d3af739709db9246e7933c683
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-13 14:56:44 +0100

    Add can send msg of multiboard

--- Byggern/Byggern/src/exercises/ex07.c
+++ Byggern/Byggern/src/exercises/ex07.c
@@ -20,12 +20,31 @@
 #include "../drivers/spi.h"
 #include "../drivers/CAN.h"
 #include "../multiboard/joystick.h"
+#include "../multiboard/slider.h"
+#include "../multiboard/button.h"
 
 
 #include <avr/interrupt.h>
 #include "ex07.h"
 
 
+struct can_message joystick_message =
+{
+	.id = 0x10,
+	.length = 3,
+};
+
+struct can_message slider_message =
+{
+	.id = 0x11,
+	.length = 2
+};
+
+struct can_message button_message =
+{
+	.id = 0x12,
+	.length = 2
+};
 
 
 
@@ -33,16 +52,29 @@ void ex07()
 {
 	can_init();
 	joystick_calib();
-	struct can_message joystick_message = 
-	{
-		.id = 42,
-		.length = 1,
-		.data.i8[0] = 0,
-	};
 	
 	while(1)
 	{
-		joystick_message.data.i8[0] = joystick_getPos(POS_X)/* * 0.05 + joystick_message.data.i8[0] * 0.95*/;
+		joystick_message.data.i8[0] = joystick_getPos(POS_X);
+		joystick_message.data.i8[1] = joystick_getPos(POS_Y); 
+		joystick_message.data.u8[2] = button_getStat(BUTTON_JOYSTICK);
 		can_message_send(&joystick_message);
+		//printf("%d\n", joystick_message.data.i8[0]);
+		
+		_delay_ms(10);
+		
+		slider_message.data.u8[0] = slider_getPos(SLIDER_LEFT);
+		slider_message.data.u8[1] = slider_getPos(SLIDER_RIGHT);
+		can_message_send(&slider_message);
+		printf("%d\n", slider_message.data.u8[1]);	
+		
+		_delay_ms(10);
+		
+		button_message.data.u8[0] = button_getStat(BUTTON_LEFT);
+		button_message.data.u8[1] = button_getStat(BUTTON_RIGHT);
+		can_message_send(&button_message);
+//		printf("%d\n", button_message.data.u8[0]);
+		
+		_delay_ms(10);
 	}	
 }
