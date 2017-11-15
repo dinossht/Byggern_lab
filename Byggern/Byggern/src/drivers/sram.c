da6f61efd27b7f1eaee6184d1caaf949aa63505c
Unknown <hermann_sun@hotmail.com>
2017-11-15 18:22:55 +0100

    Remove debug code

--- Byggern/Byggern/src/drivers/sram.c
+++ Byggern/Byggern/src/drivers/sram.c
@@ -18,25 +18,7 @@ void sram_write(uint8_t data, uint16_t memory_index)
 {
 	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM	
 	
-	uint16_t write_errors = 0;
-	
-	printf("Starting SRAM writing...\n");
 	ext_ram[memory_index] = data;
-	uint8_t retrieved_data = ext_ram[memory_index];
-	
-	while(data != retrieved_data)
-	{
-		ext_ram[memory_index] = data;
-		retrieved_data = ext_ram[memory_index];
-		
-		printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", memory_index, retrieved_data, data);
-		
-		write_errors++;
-		if(write_errors > SRAM_TIMEOUT_COUNTER)
-			printf("Transfer timed out...");
-			break;
-	}
-	printf("Data [%02X] is written", retrieved_data);
 }
 
 /* memory_loc is sram memory address index, [0, 2048] == 0x000->0x800 */
@@ -44,10 +26,7 @@ uint8_t sram_read(uint16_t memory_index)
 {
 	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
 	
-	printf("Starting SRAM reading...\n");
-	
 	uint8_t retrieved_data = ext_ram[memory_index];
-	printf("Data [%02X] is read", retrieved_data);
 	
 	return retrieved_data;
 }
