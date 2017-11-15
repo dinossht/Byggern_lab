e0ffe369e2bf113a4926e4dd7685ba727086b31d
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-15 21:43:21 +0100

    Add PID_TUNING message

--- Byggern/Byggern/src/CAN/CAN_messages.c
+++ Byggern/Byggern/src/CAN/CAN_messages.c
@@ -3,32 +3,39 @@
  *
  * Created: 11/15/2017 5:17:22 PM
  *  Author: dinos
- */ 
- #include "CAN_messages.h"
- 
- 
- //Multiboard
- struct can_message joystick_message =
- {
+*/ 
+#include "CAN_messages.h"
+
+
+//Multiboard
+struct can_message joystick_message =
+{
 	 .id = CAN_MULTIBOARD_JOYSTICK_ID,
 	 .length = 3,
- };
+};
 
- struct can_message slider_message =
- {
+struct can_message slider_message =
+{
 	 .id = CAN_MULTIBOARD_SLIDER_ID,
 	 .length = 2,
- };
+};
 
- struct can_message button_message =
- {
+struct can_message button_message =
+{
 	 .id = CAN_MULTIBOARD_BUTTON_ID,
 	 .length = 2,
- };
+};
 
- //States
- struct can_message FSM_setNode2State_message=
- {
+//States
+struct can_message FSM_setNode2State_message =
+{
 	 .id = CAN_STATE_FROMNODE1_ID,
-	 .length = 0x1,
- };
+	 .length = 1,
+};
+
+//Data
+struct can_message game_parameterTuning =
+{
+	 .id = CAN_PID_TUNING_ID,
+	 .length = 3,
+};
