cbc2a87b18fc17cdcd195f002b6d6f307426663a
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-15 19:21:59 +0100

    Add CAN folder and moved related files into it, also add rudamentary CAN_wrapper

---
+++ Byggern/Byggern/src/CAN/CAN_wrapper.c
@@ -0,0 +1,30 @@
+/*
+ * CAN_wrapper.c
+ *
+ * Created: 11/15/2017 6:51:15 PM
+ *  Author: dinos
+ */ 
+
+#include <stdint.h>
+#include <asf.h>
+#include <stdio.h>
+#include "../drivers/CAN.h"
+#include "CAN_wrapper.h"
+#include "CAN/CAN_definitions.h"
+#include "../FSM.h"
+
+
+
+void CAN_wrapper_updateStates()
+{
+	struct can_message recieveMessage;
+
+	
+	recieveMessage = can_message_recieve();
+	switch(recieveMessage.id)
+	{
+	case CAN_STATE_FROMNODE2_ID:
+		FSM_setGlobalState(recieveMessage.data.u8[0]);
+	break;
+	}
+}
