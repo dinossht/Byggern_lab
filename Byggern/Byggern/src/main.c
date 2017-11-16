e4754e9ce432dd0bd1cf4a4975cd84dccaef2a15
Unknown <hermann_sun@hotmail.com>
2017-11-16 20:59:18 +0100

    Add TODO comment

--- Byggern/Byggern/src/main.c
+++ Byggern/Byggern/src/main.c
@@ -34,10 +34,45 @@ int main (void)
 	
 	while(1)
 	{
-		joystick_message.data.i8[0] = joystick_getPos(POS_X);
-		joystick_message.data.i8[1] = joystick_getPos(POS_Y); 
-		joystick_message.data.u8[2] = button_getStat(BUTTON_JOYSTICK);
-		can_message_send(&joystick_message);
+		//Should create a function that does everything tied to updating states, both reading CAN and reading multiboard
+		CAN_wrapper_updateStates();
+		switch(FSM_getGlobalState())
+		{
+			case PLAYING:
+				joystick_message.data.i8[0] = joystick_getPos(POS_X);
+				joystick_message.data.i8[1] = joystick_getPos(POS_Y); 
+				joystick_message.data.u8[2] = button_getStat(BUTTON_JOYSTICK);
+				can_message_send(&joystick_message);
+				
+				_delay_ms(10);
+				
+				slider_message.data.u8[0] = slider_getPos(SLIDER_LEFT);
+				slider_message.data.u8[1] = slider_getPos(SLIDER_RIGHT);
+				can_message_send(&slider_message);
+				printf("%d\n", slider_message.data.u8[1]);	
+				
+				_delay_ms(10);
+				
+				button_message.data.u8[0] = button_getStat(BUTTON_LEFT);
+				button_message.data.u8[1] = button_getStat(BUTTON_RIGHT);
+				can_message_send(&button_message);
+				
+				_delay_ms(10);
+			break;
+			
+			case(GAMEOVER):
+			#warning Automagically restarts game if you lose, shold be fixed
+				FSM_setGlobalState(PLAYING);
+			break;
+			
+			case(LOGGING):
+				
+			break;
+			
+			case (TUNING):
+				
+			break;
+		}
 		
 		_delay_ms(10);
 		
