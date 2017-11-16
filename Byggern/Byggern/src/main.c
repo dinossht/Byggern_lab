c131c3655d56daab07df2988d02b3ea538390037
Unknown <hermann_sun@hotmail.com>
2017-11-16 20:53:21 +0100

    Add testing functinality to main

--- Byggern/Byggern/src/main.c
+++ Byggern/Byggern/src/main.c
@@ -1,19 +1,58 @@
-#include <asf.h>
 #include "settings.h"
+
+#include <asf.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
 #include "hardware_init.h"
+#include "FSM.h"
+#include "CAN_messages.h"
+
+#include "drivers/mcp2515.h"
+#include "drivers/spi.h"
+#include "drivers/CAN.h"
+
+#include "multiboard/oled.h"
+#include "multiboard/joystick.h"
+#include "multiboard/slider.h"
+#include "multiboard/button.h"
 
 #include "exercises/ex01.h"
 #include "exercises/ex02.h"
 #include "exercises/ex03.h"
 #include "exercises/ex04.h"
 #include "exercises/ex05.h"
-#include "exercises/ex07.h"
+//#include "exercises/ex07.h"
+
 
 int main (void)
 {
 	hardware_init();
-
-	ex07();
+	can_init();
+	joystick_calib();
 	
+	while(1)
+	{
+		joystick_message.data.i8[0] = joystick_getPos(POS_X);
+		joystick_message.data.i8[1] = joystick_getPos(POS_Y); 
+		joystick_message.data.u8[2] = button_getStat(BUTTON_JOYSTICK);
+		can_message_send(&joystick_message);
+		
+		_delay_ms(10);
+		
+		slider_message.data.u8[0] = slider_getPos(SLIDER_LEFT);
+		slider_message.data.u8[1] = slider_getPos(SLIDER_RIGHT);
+		can_message_send(&slider_message);
+		printf("%d\n", slider_message.data.u8[1]);	
+		
+		_delay_ms(10);
+		
+		button_message.data.u8[0] = button_getStat(BUTTON_LEFT);
+		button_message.data.u8[1] = button_getStat(BUTTON_RIGHT);
+		can_message_send(&button_message);
+		
+		_delay_ms(10);
+	}	
 	return 0;
 }
