d5c53062e5c0f271466689c1333cfb2984f5a8c6
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-17 16:05:12 +0100

    Add functions in fsm

--- Byggern/Byggern/src/FSM.c
+++ Byggern/Byggern/src/FSM.c
@@ -1,35 +1,61 @@
 /*
- * game.c
+ * fsm.c
  *
- * Created: 11/15/2017 2:43:42 PM
+ * Created: 11/17/2017 3:25:22 PM
  *  Author: dinos
  */ 
 
-#include "FSM.h"
 #include "drivers/CAN.h"
 #include "CAN/CAN_messages.h"
+#include "fsm.h"
+
+
+static fsm_state_t currentState;
+
+void fsm_init(void)
+{
+	currentState = IDLE;	
+}
+
+void fsm_setCurrentState(fsm_state_t state)
+{
+	currentState = state;
+}
+
+fsm_state_t fsm_getCurrentState(void)
+{
+	return currentState;
+}
+
+
+
+
+
+
 
 
 
 static FSM_stateMachine_t node1_stateMachine;
 
 
-static void FSM_setNode1State(FSM_states new_state){
+static void FSM_setNode1State(fsm_state_t new_state){
 	node1_stateMachine.FSM_current_state = new_state;
+	
 }
 
-static void FSM_setNode2State(FSM_states new_state){
-//	FSM_setNode2State_message.data.u8[0] = new_state;
-//	can_message_send(&FSM_setNode2State_message);
-}
+// static void FSM_setNode2State(FSM_states new_state){
+// 	
+// //	FSM_setNode2State_message.data.u8[0] = new_state;
+// //	can_message_send(&FSM_setNode2State_message);
+// }
 
 
-FSM_states FSM_getGlobalState(){
+fsm_state_t FSM_getGlobalState(){
 	return node1_stateMachine.FSM_current_state;
 }
 
 
-void FSM_setGlobalState(FSM_states new_state){
+void FSM_setGlobalState(fsm_state_t new_state){
 	FSM_setNode1State(new_state);
-	FSM_setNode2State(new_state);
+	// CAN_transmit(current_state_message);
 }
--- Byggern/Byggern/src/FSM.h
+++ Byggern/Byggern/src/FSM.h
@@ -1,7 +1,7 @@
 /*
- * game.h
+ * fsm.h
  *
- * Created: 11/15/2017 2:43:27 PM
+ * Created: 11/17/2017 3:25:06 PM
  *  Author: dinos
  */ 
 
@@ -9,32 +9,31 @@
 #ifndef FSM_H_
 #define FSM_H_
 
-typedef enum{
-	MENU,
-	PLAYING,
-	LOGGING,
-	TUNING,
-}FSM_states;
-
-
+typedef enum
+{
+	IDLE,
+	GAME_PLAY,
+	DATA_LOGGING,
+	DATA_PLAYBACK,
+	TUNING
+}
+fsm_state_t;
 
 typedef struct{
-	FSM_states FSM_current_state;
+	fsm_state_t FSM_current_state;
 } FSM_stateMachine_t;
 
-void FSM_setGlobalState(FSM_states new_state);
+void fsm_init(void);
+void fsm_setCurrentState(fsm_state_t state);
+fsm_state_t fsm_getCurrentState(void);
 
-FSM_states FSM_getGlobalState(void);
+
+void FSM_setGlobalState(fsm_state_t new_state);
+
+fsm_state_t FSM_getGlobalState(void);
 
 
 
-// main:
-// If CAN_message ID == gameID
-// switch
-// message == game over: set state(GAMEOVER);
-// Switch(inputFromScreen)  
-// case playback: set state(PLAYBACK);
-// case play game: set state(playing);
-// case tuning: set state(tuning); //kanskje flett inn i playback
+
 
 #endif /* FSM_H_ */
