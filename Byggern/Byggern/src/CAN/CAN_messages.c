837b05507b3bd05b590b7ff1f557c4c1c2295244
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-17 18:34:44 +0100

    clean can

--- Byggern/Byggern/src/CAN/CAN_messages.c
+++ Byggern/Byggern/src/CAN/CAN_messages.c
@@ -5,54 +5,35 @@
  *  Author: dinos
  */ 
 
+#include "can_definitions.h"
 #include "can_messages.h"
 
+struct can_message_t joystick_message =
+{
+	.id = CAN1_MULTIBOARD_JOYSTICK_ID,
+	.length = 2,
+};
 
-//Multiboard
-// struct can_message multiboard_joystick_message =
-// {
-// 	.id = CAN_MULTIBOARD_JOYSTICK_ID,
-// 	.length = 3,
-// };
-// 
-// struct can_message multiboard_slider_message =
-// {
-// 	.id = CAN_MULTIBOARD_SLIDER_ID,
-// 	.length = 2,
-// };
-// 
-// struct can_message multiboard_button_message =
-// {
-// 	.id = CAN_MULTIBOARD_BUTTON_ID,
-// 	.length = 2,
-// };
-// 
-// 
-// 
-// //States
-// struct can_message FSM_setNode2State_message =
-// {
-// 	.id = CAN_DATA_STATE_FROMNODE1_ID,
-// 	.length = 1,
-// };
-// 
-// 
-// 
-// //Data
-// struct can_message game_parameterTuning_message =
-// {
-// 	.id = CAN_PID_TUNING_ID,
-// 	.length = 3,
-// };
-// 
-// struct can_message game_setLives_message = 
-// {
-// 	.id = CAN_SET_LIVES_ID,
-// 	.length = 1,
-// };
-// 
-// struct can_message game_setController_message =
-// {
-// 	.id = CAN_SET_CONTROLLER_ID,
-// 	.length = 1,
-// };
+struct can_message_t slider_message =
+{
+	.id = CAN1_MULTIBOARD_SLIDER_ID,
+	.length = 2,
+};
+
+struct can_message_t button_message =
+{
+	.id = CAN1_MULTIBOARD_BUTTON_ID,
+	.length = 3,
+};
+
+struct can_message_t gamesData_message =
+{
+	.id = CAN1_DATA_GAME_DATA_ID,
+	.length = 3,
+};
+
+struct can_message_t pid_message =
+{
+	.id = CAN1_DATA_PID_PARAM_ID,
+	.length = 3,
+};
--- Byggern/Byggern/src/CAN/CAN_messages.h
+++ Byggern/Byggern/src/CAN/CAN_messages.h
@@ -9,24 +9,13 @@
 #ifndef CAN_MESSAGES_H_
 #define CAN_MESSAGES_H_
 
-#include "can_definitions.h"
 #include "../drivers/can.h"
 
 
-//Multiboard
-struct can_message_t multiboard_joystick_message;
-struct can_message_t multiboard_slider_message;
-struct can_message_t multiboard_button_message;
-
-//States
-struct can_message_t FSM_setNode2State_message;
-
-//Data
-struct can_message_t game_parameterTuning_message;
-struct can_message_t game_setLives_message;
-struct can_message_t game_setController_message;
-
-
-
+struct can_message_t joystick_message;
+struct can_message_t slider_message;
+struct can_message_t button_message;
+struct can_message_t gamesData_message;
+struct can_message_t pid_message;
 
 #endif /* CAN_MESSAGES_H_ */
