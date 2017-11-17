a54b92c8330545515460faeaf08f909482d93d63
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2017-11-17 16:19:03 +0100

    Clean main

--- Byggern/Byggern/src/main.c
+++ Byggern/Byggern/src/main.c
@@ -1,19 +1,10 @@
 #include "settings.h"
-#include <asf.h>
-#include <stdio.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <asf.h>
 
 #include "drivers/timer.h"
 #include "drivers/uart.h"
 #include "drivers/latch.h"
-
-#include "FSM.h"
-#include "game.h"
-
-#include "drivers/mcp2515.h"
-#include "drivers/spi.h"
-#include "drivers/CAN.h"
 #include "drivers/menu.h"
 
 #include "multiboard/oled.h"
@@ -21,9 +12,10 @@
 #include "multiboard/slider.h"
 #include "multiboard/button.h"
 
+#include "FSM.h"
+#include "game.h"
+#include "drivers/CAN.h"
 #include "CAN/CAN_wrapper.h"
-
-#include "exercises/ex07.h"
 
 
 static void navigateMenu(joystick_dir_t joystickDirection);
@@ -85,93 +77,6 @@ int main (void)
 			break;
 		}
 	}
-		
-	while(1)
-	{
-		oled_clearScreen();
-		CAN_wrapper_updateStates();
-		game_updateControllerInput();
-		
-		
-		oled_clearScreen();
-		
-		joyPos = multiboardInputs.joystickPositionX;
-		shoot = multiboardInputs.buttonRightPressed;
-		
-		if(multiboardInputs.joystickPressed == 1)
-		{
-			menu_selectCurrentEntry();
-		}
-		
-		switch(multiboardInputs.joystickDirection)
-		{
-			case JOY_UP:
-				menu_scrollEntry(SCROLL_UP);
-				menu_incrementEntryValue(1);
-			break;
-			
-			case JOY_DOWN:
-				menu_scrollEntry(SCROLL_DOWN);
-				menu_incrementEntryValue(0);
-			break;
-			
-			case JOY_RIGHT:
-				menu_navigateToCurrentEntry();
-			break;
-			
-			case JOY_LEFT:
-				menu_navigateToPreviusMenu();
-			break;
-		}
-
-		menu_draw();
-		
-		oled_updateScreen();
-		_delay_ms(100);
-
-		
-// 		if(currenMenu == game)
-// 		{
-// 			playGame();
-// 			
-// 		}
-// 		
-
-		
-		switch(FSM_getGlobalState())
-		{
-			case GAME_PLAY:
-				//play_game();
-			#warning There is something wrong here! Screen doesnt work when something in here is not uncommented
-//				game_init(); //Transmits game data to Node2
-//				while (game_getLives() > 0) {
-//					game_transmitControllerInput();	
-//				}
-			#warning Busy waiting will fuck up evrything 
-// 		
-//				// Retrieve score
-//				// Draw game over screen
-//				if (game_insertHighscore()){
-//					//Save username and score to SRAM
-//				}
-
-			break;
-						
-			case DATA_LOGGING:
-				// Transmit data for start logging
-				// Wait for the right amount of CAN messages with logging id
-				//Save theese to buffer or SRAM or something
-				
-
-			break;
-//			
-			case TUNING:
-				game_transmitParameters();
-
-			break;
-		}
-	}	
-	return 0;
 }
 
 static void navigateMenu(joystick_dir_t joystickDirection)
